fac4648990e23193b7ae999155cdf5ddda8555c5
Shalabh Gupta <shalabhgupta@Shalabhs-MacBook-Air.local>
2021-09-07 14:39:52 +0530

    input directly in gen_graph

--- Assignment 1/gen_graph.py
+++ Assignment 1/gen_graph.py
@@ -15,4 +15,31 @@ with open("edge_list.txt",'w+') as f:
         line = str(edge[0]) + " " + str(edge[1]) + "\n"
         f.write(line)
 
-print(len(g.edges()))
+    n = input("Enter the fraction of slow nodes: ")
+    n += "\n"
+    f.write(n)
+    
+    T_tx = input("Enter mean interarrival time for transactions: ")
+    T_tx += "\n"
+    f.write(T_tx)
+
+    T_k = input("Enter mean block mining time: ")
+    T_k += "\n"
+    f.write(T_k)
+
+    
+
+    invalid_block_nodes = input("How many nodes should produce invalid blocks?")
+    invalid_block_nodes += "\n"
+    f.write(invalid_block_nodes)
+
+    for x in range(0,int(invalid_block_nodes)):
+        n = input("Enter node to produce invalid block ")
+        n += "\n"
+        f.write(n)
+    
+
+
+
+
+#print(len(g.edges()))
